acc9a1ace09c745c9dfa074ddfa7f2107cc099eb
idoavnon <39345599+idoavnon@users.noreply.github.com>
2018-05-17 22:59:32 -0700

    Add files via upload

---
+++ FloatingPoint.java
@@ -0,0 +1,22 @@
+public class FloatingPoint {
+   private double x;
+   private double y;
+   
+   public FloatingPoint(double x, double y) {
+      this.x = x;
+      this.y = y;
+   }
+   
+   public double getX() {
+      return x;
+   }
+   
+   public double getY() {
+      return y;
+   }
+   
+   public void setPoint(double x, double y) {
+      this.x = x;
+      this.y = y;
+   }
+}
