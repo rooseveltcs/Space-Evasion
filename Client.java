d0be7fbf72cdc33cb39765488b85d1bd0c651da4
idoavnon <39345599+idoavnon@users.noreply.github.com>
2018-06-02 10:46:52 -0700

    Delete Client.java

--- Client.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-import javax.swing.*;
-
-public class Client {
-   public static void main(String[] args) {
-      BoardFrame f = new BoardFrame();
-      f.createBoard();
-   }
-}
