978c1342a39ac8a85897af68e896e1c315118aef
grgimrgi <39345626+grgimrgi@users.noreply.github.com>
2018-06-04 11:07:56 -0700

    Add files via upload

---
+++ StartGame.java
@@ -0,0 +1,7 @@
+public class StartGame extends JPanel {
+   public void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      
+      https://stackoverflow.com/questions/20098124/displaying-an-image-in-a-jframe
+   }   
+}  
