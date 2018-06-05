f5402dfeefff199e93f3ba98d86a23fede4b7a62
grgimrgi <39345626+grgimrgi@users.noreply.github.com>
2018-06-05 11:21:51 -0700

    Add files via upload

--- StartGame.java
+++ StartGame.java
@@ -2,6 +2,19 @@ public class StartGame extends JPanel {
    public void paintComponent(Graphics g) {
       super.paintComponent(g);
       
-      https://stackoverflow.com/questions/20098124/displaying-an-image-in-a-jframe
+      g.setBackground(Color.BLACK);
+      g.setColor(Color.WHITE);
+      //some circle stars
+      for(int i = 1; i <= 25; i++){
+         g.drawOval(Math.random(), Math.random(), 5, 5);
+      }
+      g.setColor(Color.RED);
+      g.setFont(new Font("Impact", Font.PLAIN, 30)); 
+      g.drawString("S", 100, 200);
+      g.drawString("P", 140, 185);
+      g.drawString("A", 180, 170);
+      g.drawString("C", 220, 155);
+      g.drawString("E", 260, 140);
+      g.drawString("EVASION", 180, 200);   
    }   
 }  
